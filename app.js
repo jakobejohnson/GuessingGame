a794a6301010410a60681690b8e5a7cc684ac3b8
Johnson <jjohnson@FVFZMYBAJ1WK.tld>
2022-04-08 10:19:19 -0500

    GuessingGame2

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 function run() {
     document.getElementById("paragraph").innerHTML = "hello world!";
+    document.getElementById("paragraph").style.backgroundColor = "green";
+    document.getElementById("paragraph").style.color = "#ffffff";
+    document.getElementById("paragraph").style.padding = "20px";
 }
 
+function randomNum() {
+    let ran = Math.floor(Math.random() * 10);
+    document.getElementById("")
+}
